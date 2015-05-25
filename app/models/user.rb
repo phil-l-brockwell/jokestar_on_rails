0382ce03265d0bf317706b6b81c4e3d8ab69fe75
Phil Brockwell <nineteen40eight@gmail.com>
2015-05-25 20:22:29 +0100

    added bootstrap-sass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,26 @@ class User < ActiveRecord::Base
   DEFAULT_RANK = :snowflake
   RANKS =
     {
-      0 => :buffoon,
-      5 => :performing_monkey,
-      10 => :stooge,
-      15 => :clown,
-      20 => :jester,
-      25 => :imp,
-      30 => :funny_man,
-      35 => :kidder,
-      40 => :funster,
-      45 => :josher,
-      50 => :trickster,
-      55 => :hoaxer,
-      60 => :prankster,
-      65 => :jokester,
-      70 => :wise_cracker,
-      75 => :entertainer,
-      80 => :life_of_the_party,
-      85 => :comic,
-      90 => :joker,
-      100 => :'joke_*'
+      0   => :buffoon,
+      5   => :performing_monkey,
+      10  => :stooge,
+      15  => :clown,
+      20  => :jester,
+      25  => :imp,
+      30  => :funny_guy,
+      35  => :kidder,
+      40  => :funster,
+      45  => :josher,
+      50  => :trickster,
+      55  => :hoaxer,
+      60  => :prankster,
+      65  => :jokester,
+      70  => :wise_cracker,
+      75  => :entertainer,
+      80  => :life_of_the_party,
+      85  => :comic,
+      90  => :joker,
+      100 => :'joke*'
     }
 
   has_many :jokes, dependent: :destroy
