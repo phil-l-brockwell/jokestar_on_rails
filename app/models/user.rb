051bed1f3aa61a91eb6a1c1118f1f0eeed24b515
Phil Brockwell <nineteen40eight@gmail.com>
2015-06-04 20:44:09 +0100

    some refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,31 @@
 class User < ActiveRecord::Base
 
   after_initialize :defaults
-
   has_many :jokes, dependent: :destroy
   has_many :stars, dependent: :destroy
   has_many :starred_jokes, through: :stars, source: :joke, dependent: :destroy
   
   DEFAULT_RANK = :snowflake
-  RANKS =
-    {
-      0   => :buffoon,
-      5   => :performing_monkey,
-      10  => :stooge,
-      15  => :clown,
-      20  => :jester,
-      25  => :imp,
-      30  => :funny_guy,
-      35  => :kidder,
-      40  => :funster,
-      45  => :josher,
-      50  => :trickster,
-      55  => :hoaxer,
-      60  => :prankster,
-      65  => :jokester,
-      70  => :wise_cracker,
-      75  => :entertainer,
-      80  => :life_of_the_party,
-      85  => :comic,
-      90  => :joker,
-      100 => :'joke*'
-    }
+  RANKS = { 0   => :buffoon,
+            5   => :performing_monkey,
+            10  => :stooge,
+            15  => :clown,
+            20  => :jester,
+            25  => :imp,
+            30  => :funny_guy,
+            35  => :kidder,
+            40  => :funster,
+            45  => :josher,
+            50  => :trickster,
+            55  => :hoaxer,
+            60  => :prankster,
+            65  => :jokester,
+            70  => :wise_cracker,
+            75  => :entertainer,
+            80  => :life_of_the_party,
+            85  => :comic,
+            90  => :joker,
+            100 => :'joke*' }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -53,8 +49,8 @@ class User < ActiveRecord::Base
     self.save
   end
 
-  def decrement_rating!(points=1)
-    self.rating -= points
+  def decrement_rating!
+    self.rating -= 1
     self.save
   end
 end
